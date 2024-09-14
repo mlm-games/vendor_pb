2a73d9150dec14480beefe7b0420ef0a372e5b16
Mohd Faraz <androiabledroid@gmail.com>
2024-09-14 19:29:44 +0530

    pb: Hack for CTS
    Change-Id: Ie568c34db78ddf5d695bc1caa6b60c5fcb675ad3
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,15 @@
 alias twrppick='./vendor/pb/repopick.py -g https://gerrit.twrp.me'
+
+# CTS HACK
+CTS_FILE="cts/tests/tests/os/assets/platform_versions.txt"
+CTS_RELE="cts/tests/tests/os/assets/platform_releases.txt"
+CTS_VERSION=99.87.36
+
+if ! grep -q ${CTS_VERSION} ${CTS_FILE}; then
+	echo ${CTS_VERSION} >> ${CTS_FILE};
+fi
+
+if ! grep -q ${CTS_VERSION} ${CTS_RELE}; then
+	echo ${CTS_VERSION} >> ${CTS_RELE};
+fi
+
